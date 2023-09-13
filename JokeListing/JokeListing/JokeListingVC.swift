ce3499547bc311e64d6592ce7e86699d09fe78b1
Anushkatripathi <anushkakiet@gmail.com>
2023-09-13 16:17:16 +0530

    Changes Done

--- JokeListing/JokeListing/JokeListingVC.swift
+++ JokeListing/JokeListing/JokeListingVC.swift
@@ -11,25 +11,50 @@ import UIKit
 
 class JokeListingVC: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    // @IBOutlet weak var tableView: UITableView!
+    let tableView = UITableView()
+    let label = UILabel()
     
     let jokePresenter = JokePresenterFetching()
     var jokes: [JokeModel] = []
-
+    
     
     
     //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        self.view.backgroundColor = .black
+        
+        self.label.text = "Unlimint Jokes"
+        self.label.textAlignment = .center
+        self.label.textColor = UIColor(red: 210 / 255, green: 245 / 255, blue: 97 / 255, alpha: 1.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
         
         self.tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.backgroundColor = .black
         
+        self.view.addSubview(label)
+        self.view.addSubview(tableView)
         
         self.tableView.register(UINib(nibName:String(describing: JokeListingTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: JokeListingTableViewCell.self))
         
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            tableView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 30),
+            
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
         self.jokePresenter.delegate = self
         self.jokePresenter.JokesFetching()
-        Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { [weak self] _ in
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { [weak self] _ in
             self?.jokePresenter.JokesFetching()
         }
     }
@@ -40,9 +65,8 @@ extension JokeListingVC: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return jokes.count
-        
     }
-        
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if  let cell = tableView.dequeueReusableCell(withIdentifier: "JokeListingTableViewCell", for: indexPath) as? JokeListingTableViewCell {
@@ -52,10 +76,9 @@ extension JokeListingVC: UITableViewDataSource {
             
             return cell
         }
-        
         return UITableViewCell()
     }
-
+    
 }
 
 extension JokeListingVC: JokePresenter {
@@ -65,6 +88,4 @@ extension JokeListingVC: JokePresenter {
             self.tableView.reloadData()
         }
     }
-    
-    
 }
